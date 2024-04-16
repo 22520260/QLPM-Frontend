33a8bc0b42f39dc1fcfb223b4f295bb16d1f97c1
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2024-04-16 10:58:23 +0700

    TestAPI

--- src/component/Layout/TabLayout/ListForm/index.js
+++ src/component/Layout/TabLayout/ListForm/index.js
@@ -1,32 +1,30 @@
 function ListForm({ customers }) {
     return (
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Info</th>
-                        <th scope="col">Bill</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Khác</th>
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">City</th>
+                    <th scope="col">State</th>
+                </tr>
+            </thead>
+            <tbody>
+                {customers.map((customer, index) => ( // Đảo ngược thứ tự tham số trong map
+                    <tr key={index}>
+                        <th scope="row">{customer[0]}</th> {/* ID */}
+                        <td>{customer[1]}</td> {/* Name */}
+                        <td>{customer[2]}</td> {/* Phone */}
+                        <td>{customer[3]}</td> {/* Address */}
+                        <td>{customer[4]}</td> {/* City */}
+                        <td>{customer[5]}</td> {/* State */}
                     </tr>
-                </thead>
-                <tbody>
-                    {customers.map((customer, index) => (
-                        <tr key={index}>
-                            <th scope="row">{customer.id}</th>
-                            <td>{customer.name}</td>
-                            <td>
-                                <div>{customer.info.age} - {customer.info.gender}</div>
-                                <div>{customer.info.phone}</div>
-                            </td>
-                            <td>{customer.bill}</td>
-                            <td>{customer.status}</td>
-                            <td>{customer.other}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                ))}
+            </tbody>
+        </table>
     );
 }
+
 export default ListForm;
--- src/pages/TiepDon/tabpages/DanhSachDangKy/index.js
+++ src/pages/TiepDon/tabpages/DanhSachDangKy/index.js
@@ -1,45 +1,71 @@
-import { IFNgay, IFSearch } from "../../../../component/Layout/TabLayout/InputForm";
-import ListForm from "../../../../component/Layout/TabLayout/ListForm";
-import { useState } from "react";
-import { getCustomer, getLength } from "../../../../component/Layout/TabLayout/ListForm/data";
-import Pagination from "../../../../component/Layout/TabLayout/Pagination";
-
-function DanhSachDangKy() {
+import {
+    IFNgay,
+    IFSearch,
+  } from "../../../../component/Layout/TabLayout/InputForm";
+  import ListForm from "../../../../component/Layout/TabLayout/ListForm";
+  import { useState, useEffect } from "react";
+  import Pagination from "../../../../component/Layout/TabLayout/Pagination";
+  
+  function DanhSachDangKy() {
     const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(5);
-
-    let totalPages = Math.ceil(getLength() / limit);
-
+    const limit = 5;
+    const [customers, setCustomers] = useState([]);
+    const [displayedCustomers, setDisplayedCustomers] = useState([]);
+  
+    useEffect(() => {
+      fetch("http://localhost:3001/tiepdon")
+        .then((response) => response.json())
+        .then((data) => {
+          setCustomers(data);
+          console.log("Customers:", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching customers:", error);
+        });
+    }, []);
+  
+    useEffect(() => {
+      // Tính toán danh sách khách hàng được hiển thị trên trang hiện tại
+      const startIdx = (page - 1) * limit;
+      const endIdx = Math.min(startIdx + limit, customers.length);
+      const pageCustomers = customers.slice(startIdx, endIdx);
+      setDisplayedCustomers(pageCustomers);
+    }, [customers, page, limit]);
+  
+    const totalPages = Math.ceil(customers.length / limit);
+  
     function handlePageChange(value) {
-        if (value === "&laquo;" || value === "... ")
-            setPage(1);
-        else if (value === "&lsaquo;") {
-            if (page !== 1)
-                setPage(page - 1);
-        }
-        else if (value === "&rsaquo;") {
-            if (page !== totalPages)
-                setPage(page + 1);
-        }
-        else if (value === "&raquo;" || value === " ...") {
-            setPage(totalPages);
-        }
-        else {
-            setPage(value);
-        }
+      if (value === "&laquo;" || value === "... ") setPage(1);
+      else if (value === "&lsaquo;") {
+        if (page !== 1) setPage(page - 1);
+      } else if (value === "&rsaquo;") {
+        if (page !== totalPages) setPage(page + 1);
+      } else if (value === "&raquo;" || value === " ...") {
+        setPage(totalPages);
+      } else {
+        setPage(value);
+      }
     }
+  
     return (
-        <>
-            <div className="row py-2">
-                <IFNgay title={"Từ ngày"} />
-                <IFNgay title={"Đến ngày"} />
-                <IFSearch title={"Tìm kiếm từ khóa"} size={4} />
-            </div>
-
-            <ListForm customers={getCustomer(page, limit)} />
-            <Pagination totalPages={totalPages} page={page} limit={limit} siblings={1} onPageChange={handlePageChange} />
-        </>
+      <>
+        <div className="row py-2">
+          <IFNgay title={"Từ ngày"} />
+          <IFNgay title={"Đến ngày"} />
+          <IFSearch title={"Tìm kiếm từ khóa"} size={4} />
+        </div>
+  
+        <ListForm customers={displayedCustomers} />
+        <Pagination
+          totalPages={totalPages}
+          page={page}
+          limit={limit}
+          siblings={1}
+          onPageChange={handlePageChange}
+        />
+      </>
     );
-}
-
-export default DanhSachDangKy;
+  }
+  
+  export default DanhSachDangKy;
+  
