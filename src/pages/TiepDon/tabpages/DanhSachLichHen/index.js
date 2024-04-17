76b778088415fa28fd6c786b2cc92162f805d3f5
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2024-04-17 21:26:14 +0700

    delete data.js

--- src/pages/TiepDon/tabpages/DanhSachLichHen/index.js
+++ src/pages/TiepDon/tabpages/DanhSachLichHen/index.js
@@ -1,42 +1,67 @@
-import { IFNgay, IFSearch } from "../../../../component/Layout/TabLayout/InputForm";
+import {
+  IFNgay,
+  IFSearch,
+} from "../../../../component/Layout/TabLayout/InputForm";
 import ListForm from "../../../../component/Layout/TabLayout/ListForm";
 import { useState } from "react";
-import { getData, getLength } from "./data";
 import Pagination from "../../../../component/Layout/TabLayout/Pagination";
 import { usePaginationHandler } from "../../../../utils/appUtils";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchData } from "../../../../redux/action/actions";
 
 function DanhSachLichHen() {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data.data);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [displayedCustomers, setDisplayedCustomers] = useState([]);
 
-    const columns = [
-        { title: 'ID', key: 'id' },
-        { title: 'Ngày hẹn', key: 'date' },
-        { title: 'Info', render: (row) => (
-            <div>{row.info.name}<br />{row.info.age} - {row.info.gender}<br />{row.info.phone}</div>
-        ) },
-        { title: 'Nội dung', key: 'content' },
-        { title: 'Bác sĩ', key: 'doctor' },
-        { title: 'Status', key: 'status' },
-        { title: 'Other', key: 'other' }
-    ];
+  const columns = [
+    { title: "ID", key: "0" },
+    { title: "Ngày hẹn", key: "1" },
+    { title: "Info", key: "2" },
+    { title: "Nội dung", key: "3" },
+    { title: "Bác sĩ", key: "4" },
+    { title: "Status", key: "5" },
+    { title: "Other", key: "6" },
+  ];
 
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(5);
+  let totalPages = data ? Math.ceil(data.length / limit) : 0;
 
-    let totalPages = Math.ceil(getLength() / limit);
+  const handlePageChange = usePaginationHandler(setPage, page, totalPages);
 
-    const handlePageChange = usePaginationHandler(setPage, page, totalPages);
-    return (
-        <>
-            <div className="row py-2">
-                <IFNgay title={"Từ ngày"} />
-                <IFNgay title={"Đến ngày"} />
-                <IFSearch title={"Tìm kiếm từ khóa"} size={4} />
-            </div>
+  useEffect(() => {
+    dispatch(fetchData("http://localhost:3001/tiepdon"));
+  }, []);
 
-            <ListForm columns={columns} data={getData(page, limit)} />
-            <Pagination totalPages={totalPages} page={page} limit={limit} siblings={1} onPageChange={handlePageChange} />
-        </>
-    );
+  useEffect(() => {
+    if (data) {
+      const startIdx = (page - 1) * limit;
+      const endIdx = Math.min(startIdx + limit, data.length);
+      const pageCustomers = data.slice(startIdx, endIdx);
+      setDisplayedCustomers(pageCustomers);
+    }
+  }, [data, page, limit]);
+
+  return (
+    <>
+      <div className="row py-2">
+        <IFNgay title={"Từ ngày"} />
+        <IFNgay title={"Đến ngày"} />
+        <IFSearch title={"Tìm kiếm từ khóa"} size={4} />
+      </div>
+
+      <ListForm columns={columns} data={displayedCustomers} />
+      <Pagination
+        totalPages={totalPages}
+        page={page}
+        limit={limit}
+        siblings={1}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
 }
 
 export default DanhSachLichHen;
