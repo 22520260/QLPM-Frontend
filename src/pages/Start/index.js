2a58620f26185189a695004bfbeb654dd5aa95d4
nghgi <22520357@gm.uit.edu.vn>
2024-04-20 19:40:37 +0700

    update icon

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -7,17 +7,17 @@ function Start() {
             <Header />
             <div className="container justify-content-md-center d-flex my-5" >
                 <div className="col col-2 text-center mx-3 px-3">
-                    <StartButton title={"Tiếp đón"} path={'/tiepdon'} />
-                    <StartButton title={"Thuốc"} path={'/thuoc'} />
+                    <StartButton title={"Tiếp đón"} path={'/tiepdon'} img={'TiepDon'}/>
+                    <StartButton title={"Thuốc"} path={'/thuoc'} img={'Thuoc'}/>
 
                 </div>
                 <div className="col col-2 text-center mx-3 px-3">
-                    <StartButton title={"Khám bệnh"} path={'/khambenh'} />
-                    <StartButton title={"Dashboard"} path={'/dashboard'} />
+                    <StartButton title={"Khám bệnh"} path={'/khambenh'} img={'KhamBenh'}/>
+                    <StartButton title={"Dashboard"} path={'/dashboard'} img={'DashBoard'} />
 
                 </div>
                 <div className="col col-2 text-center mx-3 px-3">
-                    <StartButton title={"Khách hàng"} path={'/khachhang'} />
+                    <StartButton title={"Khách hàng"} path={'/khachhang'} img={'KhachHang'} />
                 </div>
 
             </div>
