a01da93bcaa681863d243e80aebc1d2936cb3e2a
nghgi <22520357@gm.uit.edu.vn>
2024-04-25 13:18:17 +0700

    Thuoc

--- src/component/Layout/TabLayout/ListForm/index.js
+++ src/component/Layout/TabLayout/ListForm/index.js
@@ -109,3 +109,56 @@ export function ListFormAddBtnThanhToanAndChiTiet({ columns, data, loading }) {
     </>
   );
 }
+
+export function ListFormThuoc({ columns, data, loading }) {
+  function handleRowClick(row) {
+    console.log(row); // Log row object when clicked
+  }
+
+  return (
+    <>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            {columns.map((column, index) => (
+              <th key={index} scope="col">
+                {column.title}
+              </th>
+            ))}
+            <th>Thao tác</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr>
+              <td colSpan={columns.length + 3}>
+                <div className="d-flex align-items-center justify-content-between">
+                  <strong>Loading...</strong>
+                  <div className="spinner-border ms-2" role="status"></div>
+                </div>
+              </td>
+            </tr>
+          ) : (
+            data.map((row, rowIndex) => (
+              <tr key={rowIndex} onClick={() => handleRowClick(row)}>
+                <td>{rowIndex + 1}</td>
+                {columns.map((column, colIndex) => (
+                  <td key={colIndex}>
+                    {column.render ? column.render(row) : row[column.key]}
+                  </td>
+                ))}
+                <td>
+                  <CTPhieuKham props={row}/>
+                  <button className="btn btn-primary ml-">
+                    <FaCommentDollar />
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+}
--- src/pages/Thuoc/index.js
+++ src/pages/Thuoc/index.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import { IFSearch } from "../../component/Layout/TabLayout/InputForm";
+
 function Thuoc() {
-    return ( 
+    const [searchKeyword, setSearchKeyword] = useState("");
+    const handleIFSearchChange = (value) => {
+        setSearchKeyword(value);
+    }
+
+    const columns = [
+        { title: "Mã phiếu", key: "0" },
+        { title: "STT", key: "1" },
+        { title: "Họ Tên", key: "2" },
+        { title: "Tên Bác sĩ", key: "3" },
+        { title: "Tổng tiền", key: "4" },
+        { title: "Trạng thái", key: "5" },
+      ];
+
+    return (
         <>
             <h1 className="container-fluid">Thuốc</h1>
+            <div className="container-fluid">
+                <div className="row py-2">
+                    <IFSearch
+                        title={"Tìm theo tên thuốc"}
+                        size={4}
+                        onChange={(value) => handleIFSearchChange(value)}
+                    />
+                    <IFSearch
+                        title={"Tìm theo tên hoạt chất"}
+                        size={4}
+                        onChange={(value) => handleIFSearchChange(value)}
+                    />
+                    <IFSearch
+                        title={"Tìm theo số lượng tồn tối đa"}
+                        size={2}
+                        onChange={(value) => handleIFSearchChange(value)}
+                    />
+                </div>
+
+
+            </div>
         </>
-     );
+    );
 }
 
 export default Thuoc;
