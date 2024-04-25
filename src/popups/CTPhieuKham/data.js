3cfd922a78f52564092343e6ac6bf5db84d84e96
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2024-04-25 12:46:19 +0700

    CTPK+TT

--- src/popups/CTPhieuKham/data.js
+++ src/popups/CTPhieuKham/data.js
@@ -6,6 +6,5 @@ import HoaDon from "./subTabs/HD";
 export const tabsDataCTPK = [
     { title: 'Thông tin khách hàng', content: 'ctpk-ttkh', component: ThongTinKhachHang },
     { title: 'Thông tin khám', content: 'ctpk-ttk', component: ThongTinKham },
-    // { title: 'Dịch vụ - Đơn thuốc', content: 'ctpk-dvk', component: DichVuDonThuoc },
     { title: 'Hóa đơn', content: 'ctpk-hd', component: HoaDon },
 ];
---
+++ src/popups/ThanhToan/subTabs/ChuaThanhToan.js
@@ -0,0 +1,9 @@
+function ChuaThanhToan() {
+    return ( 
+        <>
+            <h1 className="container-fluid">Chưa thanh toán</h1>
+        </>
+     );
+}
+
+export default ChuaThanhToan;
