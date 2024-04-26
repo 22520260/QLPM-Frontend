3f1f13b695a83d9489a2a294680709335db4484e
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2024-04-26 17:35:56 +0700

    merge Thuoc

--- src/component/Layout/DefaultLayout/Navbar/index.js
+++ src/component/Layout/DefaultLayout/Navbar/index.js
@@ -7,45 +7,76 @@ function Navbar() {
     <>
       <nav className="navbar bg-primary navbar-expand-md">
         <div className="container-fluid px-4">
-          <Link className="navbar-brand"to="../"><FaAccessibleIcon /></Link> 
+          <Link className="navbar-brand" to="../">
+            <FaAccessibleIcon />
+          </Link>
 
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link"to="/tiepdon" aria-current="page">Tiếp đón</Link> 
-          </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/tiepdon" aria-current="page">
+                Tiếp đón
+              </Link>
+            </li>
 
-          <li className="nav-item dropdown">
-            <Link className="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Khám bệnh
-            </Link> 
-            <ul className="dropdown-menu">
-              <li><Link className="dropdown-item"to="/khambenh">Khám bệnh</Link> </li>
-              <li><Link className="dropdown-item"to="/khambenh/canlamsang">Cận lâm sàng</Link> </li>
-            </ul>
-          </li>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Khám bệnh
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" to="/khambenh">
+                    Khám bệnh
+                  </Link>{" "}
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/khambenh/canlamsang">
+                    Cận lâm sàng
+                  </Link>{" "}
+                </li>
+              </ul>
+            </li>
 
-          <li className="nav-item ">
-            <Link className="nav-link"to="/khachhang" role="button" aria-expanded="false">
-              Khách hàng
-            </Link> 
-          </li>
+            <li className="nav-item ">
+              <Link
+                className="nav-link"
+                to="/khachhang"
+                role="button"
+                aria-expanded="false"
+              >
+                Khách hàng
+              </Link>
+            </li>
 
-          <li className="nav-item ">
-            <Link className="nav-link"to="/thuoc" role="button" aria-expanded="false">
-              Thuốc
-            </Link> 
-          </li>
+            <li className="nav-item ">
+              <Link
+                className="nav-link"
+                to="/thuoc"
+                role="button"
+                aria-expanded="false"
+              >
+                Thuốc
+              </Link>
+            </li>
 
-          <li className="nav-item ">
-            <Link className="nav-link"to="/dashboard" role="button" aria-expanded="false">
-              Dashboard
-            </Link> 
-          </li>
+            <li className="nav-item ">
+              <Link
+                className="nav-link"
+                to="/dashboard"
+                role="button"
+                aria-expanded="false"
+              >
+                Dashboard
+              </Link>
+            </li>
           </ul>
           <NavIcon />
         </div>
       </nav>
-
     </>
   );
 }
