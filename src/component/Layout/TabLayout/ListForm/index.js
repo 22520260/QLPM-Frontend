5480ce0cd387277119c8b80c4b3affc7367f65e0
nghgi <22520357@gm.uit.edu.vn>
2024-04-26 16:04:12 +0700

    GIang oe

--- src/component/Layout/TabLayout/ListForm/index.js
+++ src/component/Layout/TabLayout/ListForm/index.js
@@ -311,20 +311,11 @@ export function ListFormThuoc({ columns, data, loading }) {
                     type="button"
                     className="btn btn-primary rounded-circle"
                     data-bs-toggle="modal"
-                    data-bs-target="#idctpk"
+                    data-bs-target="#idctlt"
                   >
                     <FaEye />
                   </button>
-                  <button
-                    type="button"
-                    className="btn btn-primary rounded-circle mx-2"
-                    data-bs-toggle="modal"
-                    data-bs-target="#idtt"
-                  >
-                    <FaDollarSign />
-                  </button>
-                  {/* <CTPhieuKham info={selectedRow} id={rowIndex}/>
-                  <ThanhToan props={selectedRow} /> */}
+                  
                 </td>
               </tr>
             ))
@@ -332,10 +323,10 @@ export function ListFormThuoc({ columns, data, loading }) {
         </tbody>
       </table>
 
-      {/* Modal ChiTietPhieuKham */}
+      {/* Modal ChiTietLoThuoc */}
       <div
         className="modal fade modal-xl"
-        id="idctpk"
+        id="idctlt"
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
@@ -379,78 +370,6 @@ export function ListFormThuoc({ columns, data, loading }) {
         </div>
       </div>
 
-      {/* Modal ThanhToan */}
-      <div
-        className="modal fade modal-xl"
-        id="idtt"
-        tabindex="-1"
-        aria-labelledby="thanhtoanModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-scrollable">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="thanhtoanModalLabel">
-                Thanh toán
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="modal-body ">
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-9">
-                    <Navtab tabsData={tabsDataTT} />
-                  </div>
-                  <div className="col-3">
-                    <ListGroupItem
-                      title={"Khách hàng"}
-                      value={"Doan Danh Du"}
-                      disable={true}
-                    />
-                    <ListGroupItem
-                      title={"Người bán"}
-                      value={"Le Thi Thanh Thao"}
-                    />
-                    <ListGroupItem title={"Ngày bán"} value={"25/4/2004"} />
-                    <ListGroupItem title={"Mã phiếu"} value={selectedRow[0]} disable={true}/>
-                    <ListGroupItem title={"Tổng tiền"} value={"4.370.000"} disable={true}/>
-                    <ListGroupItem title={"Giảm giá"} value={"0"} />
-                    <ListGroupItem title={"Thành tiền"} value={"4.370.000"} disable={true}/>
-                    <ListGroupItem
-                      title={"Phương thức TT"}
-                      value={"Tiền mặt"}
-                    />
-                    <TextArea title={"Ghi chú"} value={""} />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Đóng
-              </button>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => handleThanhToan}
-              >
-                Thanh toán
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
