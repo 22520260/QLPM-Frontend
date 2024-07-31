8b8a79d0d5aaef62ee8a1f408562c4078baaa38f
Ngyyen <ngyyenle47@gmail.com>
2024-07-31 15:26:18 +0700

    fix: delete bypass comment

--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -19,7 +19,7 @@ export const selectAction = (actionName) => {
     case "CLSBYIDPK":
       return fetchDsClsByIdAction;
     case "CTDTBYIDPK":
-      return fetchCTDTByIdAction;
+      return fetchCTDTByIdAction; // comment
     default:
       return null;
     // yet another comment
